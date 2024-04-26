b7545edeea9e16a8405730678caa09c132e4ae39
Lezvie7 <105146070+Lezvie7@users.noreply.github.com>
2024-04-27 01:11:19 +0300

    +

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+// console.log('Hello')
+
+
+
+
+
+
+// Задача номер 1
+const res = prompt('Как официально называется JS?')
+
+console.log(res)  // выводим в консоль ответ пользователя
+
+if (res === 'ECMAScript') {
+    prompt('Верно')
+} else {
+    alert ('Не знаете ECMAScript?')
+}
